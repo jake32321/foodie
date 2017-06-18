c5d0399c092a07d3548630b2ca07ba1b985ba725
System <none>
2017-06-18 06:39:57 +0000

    💿🐒 Updated with Glitch

--- skills/find_food.js
+++ skills/find_food.js
@@ -1,19 +1,29 @@
-const zomato = require('zomato');
+const zomato = require('zomato.js');
+const zClient = new zomato(process.env.zomato_api_key);
 
-var searchCrit = {};
+var internals = {
+  searchCrit: {},
+  resData: {}
+};
+
 
 module.exports = (controller) => {
   controller.hears(['food me (.*) in (.*)'], 'direct_message,direct_mention', (bot, message) => {
     bot.startConversation(message, (err, convo) => {
       // Store message data for searching for a place to eat
-      searchCrit.cusine = message.match[1];
-      searchCrit.place = message.match[2];
+      internals.searchCrit.cusine = message.match[1];
+      internals.searchCrit.place = message.match[2];
       
       convo.say('Finding somewhere for you to eat!');
     });
     
-    zomato.getLocations({query: searchCrit.place}, (err, res) => {
-      console.log(res);
-    });
+    zClient.cities({q: internals.searchCrit.place})
+      .then((cityRes) => {
+        console.log(cityRes[0].id);
+        return cityRes[0].id;
+      })
+      .then((cityId) => {
+        
+      });
   });
 };
